3f148f2b6f3b29f6d864e940ea9043b25794db50
raginda <raginda@glints.com>
2021-07-02 14:11:33 +0700

    set relative path to state.json

--- src/config_and_state.py
+++ src/config_and_state.py
@@ -69,7 +69,9 @@ def update_final_state_file(endpoint):
 
     last_updated_staging_item = get_state_item(endpoint, 'last_updated_staging')
 
-    with open('state.json', 'w+') as state_file:
+    path_to_state = os.path.join(os.path.dirname(__file__), '../state.json')
+
+    with open(path_to_state, 'w+') as state_file:
         state_items["bookmarks"][endpoint]['last_updated_final'] = last_updated_date_added_1_second(last_updated_staging_item)
         state_file.write(json.dumps(state_items))
 
@@ -85,9 +87,11 @@ def update_staging_state_file(endpoint, row_data):
     # retrieve last_updated attribute name for selected endpoint
     last_updated = get_last_updated_attribute(endpoint)
 
+    path_to_state = os.path.join(os.path.dirname(__file__), '../state.json')
+
     if last_updated in row_data:
         if row_data[last_updated] > get_state_item(endpoint, "last_updated_staging"):
-            with open('state.json', 'w+') as state_file:
+            with open(path_to_state, 'w+') as state_file:
                 state_items["bookmarks"][endpoint]['last_updated_staging'] = row_data[last_updated]
                 state_file.write(json.dumps(state_items))
 
